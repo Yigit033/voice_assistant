803318c0917a57224a00054a61e0293d8fb39e8c
Yiğitcan TİLAVER <93970667+Yigit033@users.noreply.github.com>
2024-08-25 02:58:09 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_weather(city):
-    api_key = "4286bf664230f420a343cdfa07541d16"  # OpenWeatherMap API anahtarınızı buraya ekleyin
+    api_key = "*******************"  # OpenWeatherMap API anahtarınızı buraya ekleyin
     # url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     url = f"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={api_key}&units=metric"
     response = requests.get(url)
